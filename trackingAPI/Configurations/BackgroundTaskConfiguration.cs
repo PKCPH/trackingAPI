1ae619db52851de36f084952aeacdb6114754aca
Patrick Piker <patrick@klingaa.dk>
2023-02-10 12:29:18 +0100

    If statement for matches not to be created in the past but added with one further day.

--- trackingAPI/Configurations/BackgroundTaskConfiguration.cs
+++ trackingAPI/Configurations/BackgroundTaskConfiguration.cs
@@ -5,15 +5,17 @@ public static class BackgroundTaskConfiguration
     public static bool IsEnabled { get; set; } = true;
 
     //The timespan in hours of the random scheduled time of a match //use x*60 for hours.
-    public static int ScheduledTimeSpanInMinutes { get; set; } = 5;
+    public static int ScheduledTimeSpanInMinutes { get; set; } = 8*60;
     
     //Determine the earliest time of the day for the ScheduledTimeSpanInHours
-    public static int StartHourOfScheduledTimeSpan { get; set; } = 0;
+    public static int StartHourOfScheduledTimeSpan { get; set; } = 12;
 
     //Datetime of which day the match is playing at (for now the same day as created)
     //using DateTime.Now for testing, but use DateTime.Today when not testing
-    public static DateTime DateTimeStartingPoint { get; set; } = DateTime.Now;
+    public static DateTime DateTimeStartingPoint { get; set; } = DateTime.Today;
 
     //How often the backgroundtask is checking for changes.
     public static TimeSpan TimerTickTimeSpan { get; set; } = TimeSpan.FromSeconds(60);
+
+    public static DateTime CustomLocalTime { get; set; } = DateTime.UtcNow.AddHours(1);
 }
--- trackingAPI/Helpers/DateTimePicker.cs
+++ trackingAPI/Helpers/DateTimePicker.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using trackingAPI.Configurations;
+using static trackingAPI.Configurations.BackgroundTaskConfiguration;
 
 namespace trackingAPI.Helpers
 {
@@ -8,17 +9,18 @@ namespace trackingAPI.Helpers
         public static DateTime CreateRandomMatchTime()
         {
             var rnd = new Random();
+            var date = DateTimeStartingPoint;
+
             //Timespan of the random scheduled time 
-            var minutes = rnd.Next(0, BackgroundTaskConfiguration.ScheduledTimeSpanInMinutes);
+            var minutes = rnd.Next(0, ScheduledTimeSpanInMinutes);
             //when the match starts the earliest on a given day
-            var timeOfDayHours = TimeSpan.FromHours(BackgroundTaskConfiguration.StartHourOfScheduledTimeSpan);
+            var timeOfDayHours = TimeSpan.FromHours(StartHourOfScheduledTimeSpan);
             timeOfDayHours += TimeSpan.FromMinutes(minutes);
+            var pickedDateTime = date + timeOfDayHours;
+       
+            //if pickedDateTime is before CustomLocalTime.TimeOfDay return with +1 day else return
+            return (Convert.ToDateTime(CustomLocalTime) > pickedDateTime) ? pickedDateTime.AddDays(1) : pickedDateTime;
 
-            var dt2 = BackgroundTaskConfiguration.DateTimeStartingPoint + timeOfDayHours;
-
-            var dt = DateTime.Now + timeOfDayHours;
-
-            return dt;
         }
     }
 }
