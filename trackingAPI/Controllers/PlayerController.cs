d5f3ea00dec917e83e1bcf473970734d5d15ca81
Alex <alexravn97@gmail.com>
2023-02-15 08:39:50 +0100

    Player C# CRUD Done

--- trackingAPI/Controllers/PlayerController.cs
+++ trackingAPI/Controllers/PlayerController.cs
@@ -16,11 +16,62 @@ namespace trackingAPI.Controllers
             this.databaseContext = databaseContext;
         }
 
+        //Read All
         [HttpGet]
-        public async Task<IActionResult> GetAllEmployees()
+        public async Task<IActionResult> GetAllPlayers()
         {
-            var employees = await databaseContext.Players.ToListAsync();
-            return Ok(employees);
+            var players = await databaseContext.Players.ToListAsync();
+            return Ok(players);
+        }
+
+        //Create
+        [HttpPost]
+        public async Task<IActionResult> AddPlayer([FromBody] Player playerRequest)
+        {
+            playerRequest.Id = Guid.NewGuid();
+            await this.databaseContext.Players.AddAsync(playerRequest);
+            await this.databaseContext.SaveChangesAsync();
+
+            return Ok(playerRequest);
+        }
+
+        //Read one
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetPlayer([FromRoute] Guid id)
+        {
+            var player = await databaseContext.Players.FirstOrDefaultAsync(x => x.Id == id);
+            if (player == null) { return NotFound(); }
+            return Ok(player);
+        }
+
+        //Update
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdatePlayer([FromRoute] Guid id, Player updatePlayerRequest)
+        {
+            var player = await this.databaseContext.Players.FindAsync(id);
+            if (player == null) { return NotFound(); }
+            player.Name = updatePlayerRequest.Name;
+            player.age = updatePlayerRequest.age;
+            player.Team = updatePlayerRequest.Team;
+            player.TeamId = updatePlayerRequest.TeamId;
+
+            await this.databaseContext.SaveChangesAsync();
+            return Ok(player);
+        }
+
+        //Delete
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeletePlayer([FromRoute] Guid id)
+        {
+            var player = await this.databaseContext.Players.FindAsync(id);
+            if (player == null) { return NotFound(); }
+
+            this.databaseContext.Players.Remove(player);
+            await this.databaseContext.SaveChangesAsync();
+            return Ok(player);
         }
     }
 }
