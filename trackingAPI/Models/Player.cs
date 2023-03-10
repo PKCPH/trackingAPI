228b819b443c211cc86f7c136a104359ee84e70e
Alex <alexravn97@gmail.com>
2023-03-10 12:52:20 +0100

    Added more player stats

--- trackingAPI/Models/Player.cs
+++ trackingAPI/Models/Player.cs
@@ -10,7 +10,21 @@ namespace trackingAPI.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public int age { get; set; }
-        public ICollection<PlayerTeam> Teams { get; set; }
+        public int Age { get; set; }
+        public ICollection<PlayerTeam> Teams { get; set; }        
+        //number of goals
+        public int Goals { get; set; }
+        //number of goals assisted
+        public int Assists { get; set; }
+        //Yellow Cards
+        public int Yellow { get; set; }
+        //Red Cards
+        public int Red { get; set; }
+        //Set piece goal
+        public int SpG { get; set; }
+        //penalties Scored
+        public decimal PSPercent { get; set; }
+        //Man of the match
+        public int Motm { get; set; }
     }
 }
