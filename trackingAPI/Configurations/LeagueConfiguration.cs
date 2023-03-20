2b9d815819d8413dd5e8f78027b3fbd780fd0c37
Patrick Piker <patrick@klingaa.dk>
2023-03-20 12:48:44 +0100

    changed Configurations

--- trackingAPI/Configurations/LeagueConfiguration.cs
+++ trackingAPI/Configurations/LeagueConfiguration.cs
@@ -3,8 +3,8 @@
     public static class LeagueConfiguration
     {
         //How long interval between matches should be set
-        public static DateTime IntervalBetweenMatches => IntervalBetweenMatches.AddSeconds(550);
+        //public static DateTime IntervalBetweenMatches => IntervalBetweenMatches.AddSeconds(550);
 
-        public static int AmountOfTeams = 6;
+        public static int AmountOfTeams = 16;
     }
 }
--- trackingAPI/Configurations/LiveGamematchConfiguration.cs
+++ trackingAPI/Configurations/LiveGamematchConfiguration.cs
@@ -2,17 +2,17 @@
 {
     public class LiveGamematchConfiguration
     {
-        //public static int GamematchLengthInSeconds { get; set; } = 5400;
-        //public static int HalfTimeBreakLengthInMilliSeconds { get; set; } = 180000;
-        //public static int OvertimeLengthInSeconds { get; set; } = 1800;
-        //public static int PenaltyShootoutTimeIntervalInSeconds { get; set; } = 60;
+        public static int GamematchLengthInSeconds { get; set; } = 5400;
+        public static int HalfTimeBreakLengthInMilliSeconds { get; set; } = 180000;
+        public static int OvertimeLengthInSeconds { get; set; } = 1800;
+        public static int PenaltyShootoutTimeIntervalMilliInSeconds { get; set; } = 60;
 
 
-        ///////////////FOR TESTING////////////////////
-        public static int GamematchLengthInSeconds { get; set; } = 20;
-        public static int HalfTimeBreakLengthInMilliSeconds { get; set; } = 10000;
-        public static int OvertimeLengthInSeconds { get; set; } = 10;
-        public static int PenaltyShootoutTimeIntervalMilliInSeconds { get; set; } = 200;
+        /////////////////FOR TESTING////////////////////
+        //public static int GamematchLengthInSeconds { get; set; } = 20;
+        //public static int HalfTimeBreakLengthInMilliSeconds { get; set; } = 10000;
+        //public static int OvertimeLengthInSeconds { get; set; } = 10;
+        //public static int PenaltyShootoutTimeIntervalMilliInSeconds { get; set; } = 2000;
 
     }
 }
