7aa29e98bc9ddcf96ff8eca2a8b8edbba59ce4e3
Patrick Piker <patrick@klingaa.dk>
2023-03-23 10:12:15 +0100

    config changes

--- trackingAPI/Configurations/BackgroundTaskConfiguration.cs
+++ trackingAPI/Configurations/BackgroundTaskConfiguration.cs
@@ -23,7 +23,7 @@ public static class BackgroundTaskConfiguration
     public static bool IsEnabled { get; set; } = true;
 
     //The timespan in hours of the random scheduled time of a match //use x*60 for hours.
-    public static int ScheduledTimeSpanInMinutes { get; set; } = 20;
+    public static int ScheduledTimeSpanInMinutes { get; set; } = 4;
 
     //Determine the earliest time of the day for the ScheduledTimeSpanInHours
     public static int StartHourOfScheduledTimeSpan { get; set; } = 0;
