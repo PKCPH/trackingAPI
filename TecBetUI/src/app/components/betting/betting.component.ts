06874d38812bf2a97698c914fc5cb47c903b6df7
Patrick Piker <patrick@klingaa.dk>
2023-04-26 14:42:45 +0200

    trying to implement a livematch detail to section2 -wanna show league, player stats and betting on front page as a one pager

--- TecBetUI/src/app/components/betting/betting.component.ts
+++ TecBetUI/src/app/components/betting/betting.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription, interval, switchMap } from 'rxjs';
+import { LoginModel } from 'src/app/models/login.model';
+import { Match } from 'src/app/models/matches.model';
+import { Team } from 'src/app/models/teams.model';
+import { LoginService } from 'src/app/services/login.service';
+import { MatchesService } from 'src/app/services/matches.service';
 
 @Component({
   selector: 'app-betting',
@@ -6,5 +14,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./betting.component.css']
 })
 export class BettingComponent {
-
+  startTime: Date | any;
+  stopwatch: string | any;
+  matchDetails: Match | any;
 }
+
